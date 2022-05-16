4ec15fee33ff6b82540eeace323a201e98f2d584
Małgorzata Kopydłowska <niezapominajka@kodilla.com>
2022-05-16 18:45:25 +0200

    Add fix API

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-redeclare
-/* global Handlebars, utils, dataSource */
+/*global Handlebars, utils,*/
 
 {
   'use strict';
@@ -498,7 +498,8 @@
       const thisApp = this;
 
       for (let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+        //new Product(productData, thisApp.data.products[productData]);
       }
     },
 
@@ -506,6 +507,22 @@
       const thisApp = this;
 
       thisApp.data = {};
+      const url = settings.db.url + '/' + settings.db.products;
+
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse:', parsedResponse);
+
+          /* save parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+
+          /* execute initMenu method */
+          thisApp.initMenu();
+        });
+      console.log('thisApp.data', JSON.stringify(thisApp.data));
 
     },
 
